b47c4cf8a477c9b3356c2f7247166a9e3f199960
Atick Faisal <atickfaisal@gmail.com>
2020-12-28 19:03:19 +0600

    how sum optimized

--- src/Main.kt
+++ src/Main.kt
@@ -11,7 +11,7 @@ fun main() {
         // n = GridTraveller.getOptimizedNumberOfPaths(18, 18)
         // n = GridTraveller.getMoreOptimizedNumberOfPaths(18, 18)
         // n = CanSum.isPossibleOptimized(300, arrayOf(7, 14))
-       n = HowSum.getElements(300, arrayOf(7, 14))
+       n = HowSum.getElementsOptimized(8, arrayOf(2, 3, 5))
     }
 
     print("ANSWER: $n, TIME: $timeElapsed ms")
--- src/sum/HowSum.kt
+++ src/sum/HowSum.kt
@@ -15,4 +15,25 @@ object HowSum {
         }
         return null
     }
+
+    fun getElementsOptimized(
+            targetSum: Int,
+            numbers: Array<Int>,
+            memory: HashMap<Int, MutableList<Int>?> = hashMapOf()
+    ): MutableList<Int>? {
+        if (memory.containsKey(targetSum)) return memory[targetSum]
+        if (targetSum == 0) return mutableListOf()
+        if (targetSum < 0) return null
+        for (number in numbers) {
+            val newTarget = targetSum - number
+            val result = getElementsOptimized(newTarget, numbers, memory)
+            result?.let {
+                it.add(number)
+                memory[targetSum] = it
+                return it
+            }
+        }
+        memory[targetSum] = null
+        return null
+    }
 }
